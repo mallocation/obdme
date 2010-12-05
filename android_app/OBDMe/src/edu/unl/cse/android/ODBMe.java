f7b2d4ec93921e3e3c7d4fd109d3a5f307915672
Brett Baumert <bbaumert@gmail.com>
2010-12-05 19:41:24 +0000

    AND-1 Initial commit of OBDMe andriod application

---
+++ android_app/OBDMe/src/edu/unl/cse/android/ODBMe.java
@@ -0,0 +1,13 @@
+package edu.unl.cse.android;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class ODBMe extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
