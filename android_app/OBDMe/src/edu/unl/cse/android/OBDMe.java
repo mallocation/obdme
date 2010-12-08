b55f52a0c40ba910fcfa733f64e53bcd95a4bc86
Brett Baumert <bbaumert@gmail.com>
2010-12-08 18:43:17 +0000

    AND-1 Added a method to deal with the Send button

--- android_app/OBDMe/src/edu/unl/cse/android/OBDMe.java
+++ android_app/OBDMe/src/edu/unl/cse/android/OBDMe.java
@@ -1,13 +1,31 @@
 package edu.unl.cse.android;
 
 import android.app.Activity;
+import android.content.Context;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class OBDMe extends Activity {
-    /** Called when the activity is first created. */
+    
+	private EditText commandEditTextField;
+	private TextView outputTextView;
+	
+	/** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+        
+        commandEditTextField = (EditText)findViewById(R.id.commandEditText);
+    }
+    
+    public void sendButtonMethod(View view) {
+    	Context context = getApplicationContext();
+    	CharSequence text = commandEditTextField.getText();
+    	
+    	commandEditTextField.setText("");
     }
 }
