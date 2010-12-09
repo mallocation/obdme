6ab488ecd6179ce637f58e6c9883c5573341ce79
Brett Baumert <bbaumert@gmail.com>
2010-12-09 21:05:40 +0000

    AND-1 Changed back.xml in hdpi and ldpi to match the mdpi back.xml file.  Changed the manifest: added bluetooth permissions and set debuggable = true.  Added code to OBDME.java to prompt user to turn on bluetooth when the app is launched if bluetooth isn't turned on initially.

--- android_app/OBDMe/src/edu/unl/cse/android/OBDMe.java
+++ android_app/OBDMe/src/edu/unl/cse/android/OBDMe.java
@@ -1,17 +1,19 @@
 package edu.unl.cse.android;
 
 import android.app.Activity;
+import android.bluetooth.BluetoothAdapter;
 import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
-import android.widget.Toast;
 
 public class OBDMe extends Activity {
     
 	private EditText commandEditTextField;
 	private TextView outputTextView;
+	BluetoothAdapter bluetooth = BluetoothAdapter.getDefaultAdapter();
 	
 	/** Called when the activity is first created. */
     @Override
@@ -20,6 +22,12 @@ public class OBDMe extends Activity {
         setContentView(R.layout.main);
         
         commandEditTextField = (EditText)findViewById(R.id.commandEditText);
+        
+        // Prompt the user to turn on Bluetooth if it is not enabled already
+        if (!bluetooth.isEnabled()) {
+        	String enableBT = BluetoothAdapter.ACTION_REQUEST_ENABLE;
+        	startActivityForResult(new Intent(enableBT), 0);
+        }
     }
     
     public void sendButtonMethod(View view) {
