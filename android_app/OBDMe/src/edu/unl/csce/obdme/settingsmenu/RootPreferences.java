d6a4275cd37751a7d3be52bcb2a1104193ce94cb
Michael Kelly <michael@mikeryankelly.com>
2011-02-14 02:37:24 +0000

    AND-8 Started a demo of the PreferenceActivity Class

---
+++ android_app/OBDMe/src/edu/unl/csce/obdme/settingsmenu/RootPreferences.java
@@ -0,0 +1,155 @@
+package edu.unl.csce.obdme.settingsmenu;
+
+import android.os.Bundle;
+import android.preference.CheckBoxPreference;
+import android.preference.Preference;
+import android.preference.PreferenceActivity;
+import android.preference.PreferenceCategory;
+import android.preference.PreferenceScreen;
+import android.view.ViewGroup.LayoutParams;
+import android.view.Window;
+import android.widget.LinearLayout;
+import android.widget.ListView;
+import edu.unl.csce.obdme.R;
+
+/**
+ * The Class Preferences.
+ */
+public class RootPreferences extends PreferenceActivity {
+	 
+    /** The root view. */
+    private LinearLayout rootView;
+ 
+    /** The preference view. */
+    private ListView preferenceView;
+ 
+    /* (non-Javadoc)
+     * @see android.preference.PreferenceActivity#onCreate(android.os.Bundle)
+     */
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+ 
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        super.onCreate(savedInstanceState);
+
+        rootView = new LinearLayout(this); 
+        rootView.setLayoutParams(new LayoutParams(LayoutParams.FILL_PARENT, LayoutParams.FILL_PARENT));
+        rootView.setOrientation(LinearLayout.VERTICAL);
+       
+        preferenceView = new ListView(this);
+        preferenceView.setLayoutParams(new LayoutParams(LayoutParams.FILL_PARENT, LayoutParams.FILL_PARENT));
+        preferenceView.setId(android.R.id.list);
+        
+        PreferenceScreen screen = createPreferenceHierarchy();
+        screen.bind(preferenceView);
+        preferenceView.setAdapter(screen.getRootAdapter());
+ 
+        rootView.addView(preferenceView);
+
+        this.setContentView(rootView);
+        setPreferenceScreen(screen);
+
+    }
+
+    /**
+     * Creates the preference hierarchy.
+     *
+     * @return the preference screen
+     */
+    private PreferenceScreen createPreferenceHierarchy() {
+ 
+        PreferenceScreen root = getPreferenceManager().createPreferenceScreen(this); 
+        
+        createAccountCategory(root);
+        createApplicationCategory(root);
+        createDeviceCategory(root);
+ 
+        return root;
+ 
+    }
+    
+    /**
+     * Creates the account category.
+     *
+     * @param root the root
+     * @return the preference category
+     */
+    private PreferenceCategory createAccountCategory(PreferenceScreen root) {
+    	PreferenceCategory accountCategory = new PreferenceCategory(this);
+        accountCategory.setTitle(getResources().getString(R.string.menu_prefs_category_account));
+        root.addPreference(accountCategory);
+        
+        Preference accountPref = new Preference(this);
+        accountPref.setTitle(getResources().getString(R.string.menu_prefs_accountinfo_title));
+        accountPref.setSummary(getResources().getString(R.string.menu_prefs_accountinfo_summary));
+        accountPref.setPersistent(false);
+        accountCategory.addPreference(accountPref);
+        
+        Preference vehiclePref = new Preference(this);
+        vehiclePref.setTitle(getResources().getString(R.string.menu_prefs_vehicles_title));
+        vehiclePref.setSummary(getResources().getString(R.string.menu_prefs_vehicles_summary));
+        accountCategory.addPreference(vehiclePref);
+        
+        return accountCategory;
+    	
+    }
+    
+    /**
+     * Creates the application category.
+     *
+     * @param root the root
+     * @return the preference category
+     */
+    private PreferenceCategory createApplicationCategory(PreferenceScreen root) {
+    	
+    	PreferenceCategory applicationCategory = new PreferenceCategory(this);
+        applicationCategory.setTitle(getResources().getString(R.string.menu_prefs_category_application));
+        root.addPreference(applicationCategory);
+        
+        Preference usermodePref = new Preference(this);
+        usermodePref.setTitle(getResources().getString(R.string.menu_prefs_usermode_title));
+        usermodePref.setSummary(getResources().getString(R.string.menu_prefs_usermode_summary));
+        applicationCategory.addPreference(usermodePref);
+        
+        Preference displayPref = new Preference(this);
+        displayPref.setTitle(getResources().getString(R.string.menu_prefs_displayopts_title));
+        displayPref.setSummary(getResources().getString(R.string.menu_prefs_displayopts_summary));
+        applicationCategory.addPreference(displayPref);
+        
+        Preference collectionPref = new Preference(this);
+        collectionPref.setTitle(getResources().getString(R.string.menu_prefs_collectionopts_title));
+        collectionPref.setSummary(getResources().getString(R.string.menu_prefs_collectionopts_summary));
+        applicationCategory.addPreference(collectionPref);
+        
+        Preference uploadPref = new Preference(this);
+        uploadPref.setTitle(getResources().getString(R.string.menu_prefs_datausage_title));
+        uploadPref.setSummary(getResources().getString(R.string.menu_prefs_datausage_summary));
+        applicationCategory.addPreference(uploadPref);
+        
+        CheckBoxPreference unitsPref = new CheckBoxPreference(this);
+        unitsPref.setTitle(getResources().getString(R.string.menu_prefs_units_title));
+        unitsPref.setChecked(true); //TODO
+        unitsPref.setSummary(getResources().getString(R.string.menu_prefs_units_summary));
+        applicationCategory.addPreference(unitsPref);
+        
+        return applicationCategory;
+    	
+    }
+    
+    /**
+     * Creates the device category.
+     *
+     * @param root the root
+     * @return the preference category
+     */
+    private PreferenceCategory createDeviceCategory(PreferenceScreen root) {
+    	
+    	PreferenceCategory deviceCategory = new PreferenceCategory(this);
+        deviceCategory.setTitle(getResources().getString(R.string.menu_prefs_category_device));
+        root.addPreference(deviceCategory);
+        
+    	return deviceCategory;
+    	
+    }
+ 
+}
