b57c00a4c606e98370f5f8e993d4a1f5f85c3e1d
Curtis Johnson <curtisdavidjohnson@gmail.com>
2011-02-24 06:50:26 +0000

    WEB-2

---
+++ web_app/OBDMe_API_Platform/app/controllers/obdme/rest/controllerbase/LoggedController.java
@@ -0,0 +1,31 @@
+package controllers.obdme.rest.controllerbase;
+
+import models.obdme.Applications.ApiUsageLog;
+import models.obdme.Applications.ExternalApp;
+import play.mvc.Finally;
+
+/**
+ * The Class LoggedController.
+ */
+public class LoggedController extends SecureController {
+	
+	/**
+	 * Log api usage.
+	 * Will be called automatically by any rest api extending 
+	 * this controller class.
+	 * 
+	 * @param apikey the apikey of the calling application.
+	 */
+	@Finally
+	@SuppressWarnings("unused")
+    private static void logApiUsage(String apikey) {
+		validation.clear();
+		validation.required(apikey);
+		ExternalApp externalApp = ExternalApp.findByApiKey(apikey);
+		validation.required(externalApp);
+		if (!validation.hasErrors()) {			
+			/* Log the api usage */
+			new ApiUsageLog(externalApp, request.path, request.method, response.status, request.remoteAddress).validateAndSave();
+		}		
+    }
+}
