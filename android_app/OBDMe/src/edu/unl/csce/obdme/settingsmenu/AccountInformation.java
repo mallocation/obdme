8fbad4bb4abf20df98d7bcd56069501a6ab219e4
Brett Baumert <bbaumert@gmail.com>
2011-03-17 00:30:53 +0000

    WEB-33 Added/fixed handlers to conform with the latest API.  If the handler was previously defined, i just commented it out and added the new handler code; just in case some of the previous code is ever needed.

--- android_app/OBDMe/src/edu/unl/csce/obdme/settingsmenu/AccountInformation.java
+++ android_app/OBDMe/src/edu/unl/csce/obdme/settingsmenu/AccountInformation.java
@@ -23,6 +23,9 @@ import edu.unl.csce.obdme.OBDMeApplication;
 import edu.unl.csce.obdme.R;
 import edu.unl.csce.obdme.api.ObdMeService;
 import edu.unl.csce.obdme.api.entities.User;
+import edu.unl.csce.obdme.client.http.exception.CommException;
+import edu.unl.csce.obdme.client.http.exception.ObdmeException;
+import edu.unl.csce.obdme.client.http.handler.BasicObjectHandler;
 import edu.unl.csce.obdme.utilities.AppSettings;
 
 /**
@@ -244,8 +247,9 @@ public class AccountInformation extends Activity {
 					busyDialog = ProgressDialog.show(AccountInformation.this, "", getResources().getString(R.string.settings_account_dialog_creating), true);
 
 					//Send the request to the webservice to create this user
-					webFramework.getUsersService().createUser(emailText.getText().toString(), confirmPasswordText.getText().toString(), createAccountHander);
-
+					//webFramework.getUsersService().createUser(emailText.getText().toString(), confirmPasswordText.getText().toString(), createAccountHander);
+					webFramework.getUsersService().createUserAsync(emailText.getText().toString(), confirmPasswordText.getText().toString(), createAccountHandler);
+					
 				}
 
 				else if (ready == 3 && NEW_ACCOUNT == false) {
@@ -396,7 +400,8 @@ public class AccountInformation extends Activity {
 		//Format checking to make sure that the email address is valid (syntax wise)
 		if (emailRegEx.matcher(emailText.getText()).matches()) {
 			//Verify if the user is registered
-			webFramework.getUsersService().isUserRegistered(emailText.getText().toString(), userIsRegistered);
+			//webFramework.getUsersService().isUserRegistered(emailText.getText().toString(), userIsRegistered);
+			webFramework.getUsersService().getUserByEmailAsync(emailText.getText().toString(), userIsRegistered);
 		}
 
 		//Email address is not formatted correctly, show the red x in the email box
@@ -408,8 +413,47 @@ public class AccountInformation extends Activity {
 		}
 
 	}
+	
+	private final BasicObjectHandler<User> userIsRegistered = new BasicObjectHandler<User>(User.class) {
 
-	/** The user is registered. */
+		EditText emailText = (EditText) findViewById(R.id.settings_account_email_input);
+		TextView comfirmPasswordTitle = (TextView) findViewById(R.id.settings_account_confirmpassword);
+		EditText comfirmPasswordInput = (EditText) findViewById(R.id.settings_account_confirmpassword_input);
+		Button button = (Button) findViewById(R.id.settings_account_button);
+		
+		@Override
+		public void onCommException(String message) {
+			// TODO Auto-generated method stub
+			
+		}
+
+		@Override
+		public void onObdmeException(String message) {
+			// User does not exist
+			emailText.setCompoundDrawablesWithIntrinsicBounds(0, 0, R.drawable.green_plus, 0);
+			comfirmPasswordTitle.setVisibility(View.VISIBLE);
+			comfirmPasswordInput.setVisibility(View.VISIBLE);
+			button.setText(R.string.settings_account_button_createaccount_text);
+			NEW_ACCOUNT = true;	
+			VERIFIED_EMAIL = true;
+		}
+
+		@Override
+		public void onOperationCompleted(User result) {
+			// User exists already, so proceed with login
+			emailText.setCompoundDrawablesWithIntrinsicBounds(0, 0, R.drawable.green_tick, 0);
+			comfirmPasswordTitle.setVisibility(View.GONE);
+			comfirmPasswordInput.setVisibility(View.GONE);
+			button.setText(R.string.settings_account_button_signin_text);
+			NEW_ACCOUNT = false;
+			VERIFIED_CONFIRM_PASSWORD = true;
+			VERIFIED_EMAIL = true;
+		}
+		
+	};
+
+	/** The user is registered. NOW DEFINED ABOVE*/
+	/*
 	private final Handler userIsRegistered = new Handler() {
 		@Override
 		public void handleMessage(Message msg) {
@@ -448,8 +492,39 @@ public class AccountInformation extends Activity {
 			}
 		}
 	};
+	*/
+	
+	/** the confirm registered password handler */
+	private final BasicObjectHandler<User> confirmRegisteredPasswordHandler = new BasicObjectHandler<User>(User.class) {
 
-	/** The confirm registered password handler. */
+		EditText passwordText = (EditText) findViewById(R.id.settings_account_password_input);
+		
+		@Override
+		public void onCommException(String message) {
+			// TODO Auto-generated method stub
+			
+		}
+
+		@Override
+		public void onObdmeException(String message) {
+			// Password either doesn't exist or it is incorrect
+			passwordText.setCompoundDrawablesWithIntrinsicBounds(0, 0, R.drawable.red_x, 0);
+			VERIFIED_PASSWORD = 0;
+		}
+
+		@Override
+		public void onOperationCompleted(User result) {
+			// Password exists, and is correct
+			passwordText.setCompoundDrawablesWithIntrinsicBounds(0, 0, R.drawable.green_tick, 0);
+			//Check the confirm password again in case user goes back to fix original password to match the confirm password
+			checkConfirmPassword();
+			VERIFIED_PASSWORD = 1;
+		}
+		
+	};
+
+	/** The confirm registered password handler. NOW DEFINED ABOVE*/
+	/*
 	private final Handler confirmRegisteredPasswordHandler = new Handler() {
 		@Override
 		public void handleMessage(Message msg) {
@@ -475,8 +550,33 @@ public class AccountInformation extends Activity {
 			}
 		}
 	};
+	
+	
+	/** The create account handler */
+	private final BasicObjectHandler<User> createAccountHandler = new BasicObjectHandler<User>(User.class) {
 
-	/** The create account hander. */
+		@Override
+		public void onCommException(String message) {
+			// TODO Auto-generated method stub
+			
+		}
+
+		@Override
+		public void onObdmeException(String message) {
+			// TODO Auto-generated method stub
+			// account creation failed
+		}
+
+		@Override
+		public void onOperationCompleted(User result) {
+			// TODO Auto-generated method stub
+			// account creation successful
+		}
+		
+	};
+
+	/** The create account hander. NOW DEFINED ABOVE*/
+	/*
 	private final Handler createAccountHander = new Handler() {
 		@Override
 		public void handleMessage(Message msg) {
@@ -515,40 +615,29 @@ public class AccountInformation extends Activity {
 			}
 		}
 	};
+	*/
 
-	/** The get account credentials handler. */
-	private final Handler getAccountCredentialsHandler = new Handler() {
+	/** The get Basic Object handler. */
+	private final BasicObjectHandler<User> getAccountCredentialsHandler = new BasicObjectHandler<User>(User.class) {
+
 		@Override
-		public void handleMessage(Message msg) {
+		public void onCommException(String message) {
+			// TODO Auto-generated method stub
+			
+		}
 
-			switch (msg.what) {
+		@Override
+		public void onObdmeException(String message) {
+			// TODO Auto-generated method stub
+			
+		}
 
-			//Messsage from BT service indicating a connection state change
-			case 0:
-				if(msg.obj != null) {
-					accountValidationSuccessful((User)msg.obj);
-				} else {
-					if (busyDialog != null) {
-						busyDialog.dismiss();
-					}
-
-					//Show alert dialog, the app must exit.  This is not recoverable
-					AlertDialog.Builder builder = new AlertDialog.Builder(AccountInformation.this);
-					builder.setMessage(getResources().getString(R.string.settings_account_dialog_account_validate_failure))
-					.setCancelable(false)
-					.setPositiveButton("Ok", new DialogInterface.OnClickListener() {
-						@Override
-						public void onClick(DialogInterface dialog, int id) {
-							AccountInformation.this.finish();
-						}
-					});
-					AlertDialog alert = builder.create();
-					alert.show();
-				}
-				break;
-
-
-
+		@Override
+		public void onOperationCompleted(User result) {
+			// TODO Auto-generated method stub
+			if(getResources().getBoolean(R.bool.debug)) {
+				Log.d(getResources().getString(R.string.debug_tag_obdme),
+				"Account validation successful.");
 			}
 		}
 	};
