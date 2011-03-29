6149b9891cb3cb2a43b2690ca24b2eb12f2bbcf2
Curtis Johnson <curtisdavidjohnson@gmail.com>
2011-03-29 02:34:49 +0000

    WEB-34

--- android_app/OBDme_Api_Android_Client/src/edu/unl/csce/obdme/api/entities/graph/statistics/StatDataset.java
+++ android_app/OBDme_Api_Android_Client/src/edu/unl/csce/obdme/api/entities/graph/statistics/StatDataset.java
@@ -1,10 +1,17 @@
 package edu.unl.csce.obdme.api.entities.graph.statistics;
 
+import java.io.IOException;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 
+import org.codehaus.jackson.JsonGenerator;
+import org.codehaus.jackson.JsonProcessingException;
 import org.codehaus.jackson.annotate.JsonProperty;
+import org.codehaus.jackson.map.JsonSerializer;
+import org.codehaus.jackson.map.SerializerProvider;
+import org.codehaus.jackson.map.annotate.JsonSerialize;
 
 public class StatDataset {
 	
@@ -15,6 +22,7 @@ public class StatDataset {
 	public String email;
 	
 	@JsonProperty("timestamp")
+	@JsonSerialize(using=CustomDateSerializer.class)
 	public Date timestamp;
 	
 	public StatDataset(String email, Date timestamp) {
@@ -23,4 +31,12 @@ public class StatDataset {
 		this.datapoints = new ArrayList<StatDataPoint>();
 	}
 	
+	private static class CustomDateSerializer extends JsonSerializer<Date> {
+		@Override
+		public void serialize(Date arg0, JsonGenerator arg1, SerializerProvider arg2) throws IOException, JsonProcessingException {
+			SimpleDateFormat sdf = new SimpleDateFormat("EEE, dd MMM yyyy HH:mm:ss zzz");
+			String formattedDate = sdf.format(arg0);
+			arg1.writeString(formattedDate);			
+		}		
+	}	
 }
