9508427d19e54a9a1b3d5ccaf4dbee2a1196a545
Michael Kelly <michael@mikeryankelly.com>
2011-03-30 06:27:50 +0000

    WEB-17 Webapp changes

--- web_app/obdmeweb/public/javascripts/cp.js
+++ web_app/obdmeweb/public/javascripts/cp.js
@@ -2,16 +2,16 @@ $(document).ready(function() {
 			
 	$("div.panel_button").click(function(){
 		$("div#panel").animate({
-			height: "150px"
+			height: "115px"
 		}, 1000, "easeOutBounce");
 		$("div.panel_button").toggle();
 	
 	});	
 	
    $("div#hide_button").click(function() {
-		$("div#panel").animate({
+		$("div#panel").stop().animate({
 			height: "0px"
-		}, 1000, "easeInBack");
+		}, 500, "easeInBack");
    });	
 	
 });
