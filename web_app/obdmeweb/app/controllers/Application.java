97991c3f439d253de414cfdf5c49e00cd911117b
Curtis Johnson <curtisdavidjohnson@gmail.com>
2011-03-31 08:34:59 +0000

    WEB-34

--- web_app/obdmeweb/app/controllers/Application.java
+++ web_app/obdmeweb/app/controllers/Application.java
@@ -1,14 +1,26 @@
 package controllers;
 
-import play.*;
-import play.mvc.*;
-import models.*;
+import java.util.List;
+
+import models.obdmedb.obd.ObdPid;
+import models.obdmedb.statistics.VehicleDataPoint;
+import play.mvc.Controller;
+import play.mvc.With;
 
 @With(Secure.class)
 public class Application extends Controller {
 
-    public static void index() {
-			render();
+    public static void index() {    	
+    	List<Double> values = VehicleDataPoint.getLatestValuesForPid(ObdPid.getObdPid("01", "0C"), "1HGFA16537L077259", 64);
+    	StringBuffer sb = new StringBuffer();
+    	sb.append("[");
+    	for (Double val : values) {
+    		sb.append(val.doubleValue() + ",");
+    	}
+    	sb.deleteCharAt(sb.length() - 1);
+    	sb.append("]");
+    	String data = sb.toString();
+		render(data);
 	}
 
 }
