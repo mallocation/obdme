12e5c1d130319ac616e779e90c2ad7dcbab25306
Michael Kelly <michael@mikeryankelly.com>
2011-04-06 05:22:31 +0000

    AND-32 AND-33 Bug fixes

--- android_app/OBDme_Api_Android_Client/src/edu/unl/csce/obdme/api/entities/graph/statistics/VehicleStatPush.java
+++ android_app/OBDme_Api_Android_Client/src/edu/unl/csce/obdme/api/entities/graph/statistics/VehicleStatPush.java
@@ -17,10 +17,10 @@ import android.util.Log;
  * The Class VehicleStatPush.
  */
 public class VehicleStatPush implements Serializable {
-	
+
 	/** The Constant serialVersionUID. */
 	private static final long serialVersionUID = -7328584761603203312L;
-	
+
 	/**
 	 * Instantiates a new vehicle stat push.
 	 *
@@ -34,11 +34,11 @@ public class VehicleStatPush implements Serializable {
 	/** The stat sets. */
 	@JsonProperty("datasets")
 	public List<StatDataset> statSets;
-	
+
 	/** The VIN. */
 	@JsonProperty("vin")
 	public String VIN;
-	
+
 	/**
 	 * To json string.
 	 *
@@ -48,47 +48,57 @@ public class VehicleStatPush implements Serializable {
 		StringWriter json = new StringWriter();
 		JsonFactory jsonFactory = new JsonFactory();
 		JsonGenerator jsonGenerator = null;
-		
+
 		try {
 			jsonGenerator = jsonFactory.createJsonGenerator(json);
 		} catch (IOException e) {
 			Log.e("VehicleStatPush", e.getMessage());
 			return null;
 		}
-		
+
 		try {
-			
+
 			//Write the StatPush object
 			jsonGenerator.writeStartObject();
 			jsonGenerator.writeStringField("vin", this.VIN);
-			
+
 			//Write the DataSet objects
 			jsonGenerator.writeArrayFieldStart("datasets");
 			for (StatDataset ds: this.statSets) {
 				jsonGenerator.writeStartObject();
 				jsonGenerator.writeStringField("email", ds.email);
 				jsonGenerator.writeStringField("timestamp", DateFormat.getDateTimeInstance().format(ds.timestamp));
-				
-				
+
+
 				//VehicleLocation Object
-				jsonGenerator.writeObjectFieldStart("location");
-				jsonGenerator.writeStringField("accuracy", ds.location.accuracy);
-				jsonGenerator.writeStringField("bearing", ds.location.bearing);
-				jsonGenerator.writeStringField("altitude", ds.location.altitude);
-				jsonGenerator.writeStringField("latitude", ds.location.latitude);
-				jsonGenerator.writeStringField("longitude", ds.location.longitude);
-				jsonGenerator.writeEndObject();
-				
+				if (ds.location == null) {
+					jsonGenerator.writeNullField("location");
+				}
+				else {
+					jsonGenerator.writeObjectFieldStart("location");
+					jsonGenerator.writeStringField("accuracy", ds.location.accuracy);
+					jsonGenerator.writeStringField("bearing", ds.location.bearing);
+					jsonGenerator.writeStringField("altitude", ds.location.altitude);
+					jsonGenerator.writeStringField("latitude", ds.location.latitude);
+					jsonGenerator.writeStringField("longitude", ds.location.longitude);
+					jsonGenerator.writeEndObject();
+				}
+
 				//VehicleAcceleration Object
-				jsonGenerator.writeObjectFieldStart("acceleration");
-				jsonGenerator.writeStringField("accel_x", ds.acceleration.accel_x);
-				jsonGenerator.writeStringField("accel_y", ds.acceleration.accel_y);
-				jsonGenerator.writeStringField("accel_z", ds.acceleration.accel_z);
-				jsonGenerator.writeStringField("linear_accel_x", ds.acceleration.linear_accel_x);
-				jsonGenerator.writeStringField("linear_accel_y", ds.acceleration.linear_accel_y);
-				jsonGenerator.writeStringField("linear_accel_z", ds.acceleration.linear_accel_z);
-				jsonGenerator.writeEndObject();
-				
+				if (ds.acceleration == null) {
+					jsonGenerator.writeNullField("acceleration");
+				}
+				else {
+					jsonGenerator.writeObjectFieldStart("acceleration");
+					jsonGenerator.writeStringField("accel_x", ds.acceleration.accel_x);
+					jsonGenerator.writeStringField("accel_y", ds.acceleration.accel_y);
+					jsonGenerator.writeStringField("accel_z", ds.acceleration.accel_z);
+					jsonGenerator.writeStringField("linear_accel_x", ds.acceleration.linear_accel_x);
+					jsonGenerator.writeStringField("linear_accel_y", ds.acceleration.linear_accel_y);
+					jsonGenerator.writeStringField("linear_accel_z", ds.acceleration.linear_accel_z);
+					jsonGenerator.writeEndObject();
+				}
+
 				//Write the DataPoint objects
 				jsonGenerator.writeArrayFieldStart("datapoints");
 				for (StatDataPoint dp: ds.datapoints) {
@@ -105,46 +115,46 @@ public class VehicleStatPush implements Serializable {
 			jsonGenerator.writeEndObject();
 			jsonGenerator.close();
 			return json.toString();
-			
+
 		} catch (Exception e) {
 			Log.e("VehicleStatPush", e.getMessage());
 			return null;
 		}
-		
-//		GraphPush gp = createGraphPush();
-//		StringWriter json = new StringWriter();
-//		JsonFactory f = new JsonFactory();
-//		JsonGenerator g = null;
-//		try {
-//			g = f.createJsonGenerator(json);
-//		} catch (IOException e) {
-//			fail(e.getMessage());
-//		}		
-//		// start it out
-//		try {
-//			g.writeStartObject();			
-//			g.writeArrayFieldStart("entries");			
-//			for (GraphEntry entry : gp.getEntries()) {
-//				g.writeStartObject();				
-//				g.writeArrayFieldStart("points");				
-//				for (GraphPoint p : entry.getPoints()) {
-//					g.writeStartObject();
-//					g.writeStringField("mode", p.getMode());
-//					g.writeStringField("pid", p.getPid());
-//					g.writeStringField("value", p.getValue());
-//					g.writeEndObject();
-//				}				
-//				g.writeEndArray();
-//				g.writeEndObject();				
-//			}			
-//			g.writeEndArray();			
-//			g.writeEndObject();
-//			g.close();
-//		} catch (Exception e) {
-//			fail(e.getMessage());
-//		}
-//		Log.i("", json.toString());
-//		assertTrue(json.toString().length() > 0);
+
+		//		GraphPush gp = createGraphPush();
+		//		StringWriter json = new StringWriter();
+		//		JsonFactory f = new JsonFactory();
+		//		JsonGenerator g = null;
+		//		try {
+		//			g = f.createJsonGenerator(json);
+		//		} catch (IOException e) {
+		//			fail(e.getMessage());
+		//		}		
+		//		// start it out
+		//		try {
+		//			g.writeStartObject();			
+		//			g.writeArrayFieldStart("entries");			
+		//			for (GraphEntry entry : gp.getEntries()) {
+		//				g.writeStartObject();				
+		//				g.writeArrayFieldStart("points");				
+		//				for (GraphPoint p : entry.getPoints()) {
+		//					g.writeStartObject();
+		//					g.writeStringField("mode", p.getMode());
+		//					g.writeStringField("pid", p.getPid());
+		//					g.writeStringField("value", p.getValue());
+		//					g.writeEndObject();
+		//				}				
+		//				g.writeEndArray();
+		//				g.writeEndObject();				
+		//			}			
+		//			g.writeEndArray();			
+		//			g.writeEndObject();
+		//			g.close();
+		//		} catch (Exception e) {
+		//			fail(e.getMessage());
+		//		}
+		//		Log.i("", json.toString());
+		//		assertTrue(json.toString().length() > 0);
 	}
-	
+
 }
