9df9fb11ac5d0967f1b338eeba8950d78bd46de4
Curtis Johnson <curtisdavidjohnson@gmail.com>
2011-04-08 01:22:59 +0000

    WEB-37

--- web_app/obdmeapi/app/controllers/api/graph/Trips.java
+++ web_app/obdmeapi/app/controllers/api/graph/Trips.java
@@ -2,6 +2,7 @@ package controllers.api.graph;
 
 import models.obdmedb.User;
 import models.obdmedb.trips.Trip;
+import play.Logger;
 import play.data.validation.Email;
 import play.data.validation.Required;
 import play.i18n.Messages;
@@ -16,23 +17,24 @@ public class Trips extends Controller {
     
     public static void createTripForUser(@Email String email, @Required String alias) {
     	User operator = User.findByEmail(email);
-    	
+    	Logger.info("Starting method createTripForUser");
     	if (operator == null) {
     		renderJSON(new ApiError(Messages.get("api.error.user.notexist", email)));
     	}
     	
     	Trip t = new Trip(operator, alias);
     	t.validateAndSave();
+    	Logger.info("Trip was successfully created: " + t.getId().toString());
     	renderJSON(new api.entities.graph.Trip(t));    	
     }
     
-    public static void updateTripForUser(@Email String email, @Required Long tripId, @Required String alias) {
+    public static void updateTripForUser(@Email String email, @Required Long id, @Required String alias) {
     	User operator = User.findByEmail(email);
     	if (operator == null) {
     		renderJSON(new ApiError(Messages.get("api.error.user.notexist", email)));
     	}
     	
-    	Trip trip = Trip.getTripForUser(tripId, operator);
+    	Trip trip = Trip.getTripForUser(id, operator);
     	if (trip == null) {
     		renderJSON(new ApiError(Messages.get("api.trips.error.tripnotexist")));
     	}
