efea5c0537d85588cea62d6fe9c896111896e489
Brett Baumert <bbaumert@gmail.com>
2011-04-14 22:59:53 +0000

    WEB-43

--- web_app/obdmeweb/app/controllers/Home.java
+++ web_app/obdmeweb/app/controllers/Home.java
@@ -3,8 +3,41 @@ package controllers;
 import play.mvc.Controller;
 import play.mvc.With;
 
+import play.mvc.Before;
+import models.obdmedb.User;
+import models.obdmedb.vehicles.UserVehicle;
+import models.obdmedb.vehicles.Vehicle;
+
+import java.util.ArrayList;
+import java.util.List;
+import models.obdmedb.trips.Trip;
+
 @With(Secure.class)
 public class Home extends Controller {
+
+	@Before
+    static void setConnectedUser() {
+        if(Security.isConnected()) {
+            User user = User.find("byEmail", Security.connected()).first();
+            renderArgs.put("vehicles", UserVehicle.getVehiclesForUser(user));
+
+			List<Trip> userTrips = Trip.getLatestTripsForUser(user, 4);
+	    	List<IndexTripBinding> trips = new ArrayList<IndexTripBinding>();
+	    	for (Trip userTrip : userTrips) {
+	    		trips.add(new IndexTripBinding(userTrip.getId(), userTrip.getAlias()));
+	    	}
+			render(trips);
+        }
+    }
+
+	public static class IndexTripBinding {
+		public Long tripId;
+		public String tripName;
+		public IndexTripBinding(Long id, String name) {
+			this.tripId = id;
+			this.tripName = name;
+		}
+	}
 
     public static void index() {    	
 		render();
--- web_app/obdmeweb/app/controllers/Statistics.java
+++ web_app/obdmeweb/app/controllers/Statistics.java
@@ -3,11 +3,18 @@ package controllers;
 import play.mvc.Controller;
 import play.mvc.With;
 
+import models.obdmedb.obd.ObdPid;
+import java.util.List;
+import java.util.ArrayList;
+import java.util.Collections;
+
 @With(Secure.class)
 public class Statistics extends Controller {
 
     public static void index() {    	
-		render();
+		List<ObdPid> pidList = new ArrayList<ObdPid>();
+    	Collections.addAll(pidList, ObdPid.DEFINED_OBD_PIDS);
+    	render(pidList);
 	}
 
 }
