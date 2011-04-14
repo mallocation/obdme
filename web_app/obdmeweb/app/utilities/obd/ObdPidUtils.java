25b60fad9cb28367267f57def5a61c6a59db41d5
Curtis Johnson <curtisdavidjohnson@gmail.com>
2011-04-14 06:12:35 +0000

    WEB-41

---
+++ web_app/obdmeweb/app/utilities/obd/ObdPidUtils.java
@@ -0,0 +1,62 @@
+package utilities.obd;
+
+import java.io.IOException;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.xpath.XPath;
+import javax.xml.xpath.XPathConstants;
+import javax.xml.xpath.XPathExpression;
+import javax.xml.xpath.XPathExpressionException;
+import javax.xml.xpath.XPathFactory;
+
+import models.obdmedb.obd.ObdPid;
+
+import org.w3c.dom.Document;
+import org.xml.sax.SAXException;
+
+import play.Logger;
+import play.vfs.VirtualFile;
+
+public class ObdPidUtils { 
+	
+	private static Document loadObdProtocolFile() {
+		DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
+		dbf.setNamespaceAware(true);
+		DocumentBuilder docBuilder = null;
+		Document doc = null;
+		
+		try {
+			docBuilder = dbf.newDocumentBuilder();
+			doc = docBuilder.parse(VirtualFile.fromRelativePath("/app/files/obd/obd_protocol.xml").getRealFile());
+			return doc;
+		} catch (ParserConfigurationException e) {
+			Logger.error(e, "Shit, problem creating document builder in obd protocol utils.");
+			return null;
+		} catch (SAXException e) {
+			Logger.error(e, "Problem loading obd protcol.");
+			return null;
+		} catch (IOException e) {
+			Logger.error(e, "Problem loading obd protcol.");
+			return null;
+		}		
+	}
+	
+	
+	
+	public static String getPidUnit(ObdPid obdPid) {
+		XPathFactory xpathFact = XPathFactory.newInstance();
+		XPath xpath = xpathFact.newXPath();
+		
+		String unitXPathExpression = "//mode[@hex='%s']/pid[@hex='%s']/@unit";
+		try {
+			XPathExpression unitExpression = xpath.compile(String.format(unitXPathExpression, obdPid.getMode(), obdPid.getPid()));
+			return unitExpression.evaluate(loadObdProtocolFile(), XPathConstants.STRING).toString();
+		} catch (XPathExpressionException e) {
+			Logger.error(e, "Problem compiling unit xpath expression in obd protocol.");
+			return null;
+		}
+	}
+
+}
---
+++ web_app/obdmeweb/app/utilities/obd/UnitConversion.java
@@ -0,0 +1,117 @@
+package utilities.obd;
+
+
+/**
+ * The Class UnitConversion.
+ */
+public class UnitConversion {
+
+	/**
+	 * Convert to english.
+	 *
+	 * @param unit the unit
+	 * @param value the value
+	 * @return the double
+	 */
+	public static Double convertToEnglish(String unit, Double value) {
+
+		if (unit.equals("C")) {
+			return cToF(value);
+		}
+		else if (unit.equals("kPa")) {
+			return kpaToPsi(value);
+		}
+		else if (unit.equals("km/h")) {
+			return kphToMph(value);
+		}
+		else if (unit.equals("km")) {
+			return kmToMi(value);
+		}
+		else if (unit.equals("Pa")) {
+			return paToPsi(value);
+		}
+		else {
+			return value;
+		}
+
+	}
+	
+	/**
+	 * Gets the english unit.
+	 *
+	 * @param unit the unit
+	 * @return the english unit
+	 */
+	public static String getEnglishUnit(String unit) {
+
+		if (unit.equals("\u00B0C")) {
+			return "\u00B0F";
+		}
+		else if (unit.equals("kPa")) {
+			return "psi";
+		}
+		else if (unit.equals("km/h")) {
+			return "mph";
+		}
+		else if (unit.equals("km")) {
+			return "mi";
+		}
+		else if (unit.equals("Pa")) {
+			return "psi";
+		}
+		else {
+			return unit;
+		}
+
+	}
+
+	/**
+	 * C to f.
+	 *
+	 * @param value the value
+	 * @return the double
+	 */
+	public static Double cToF(Double value) {
+		return (9.0 / 5.0 ) * value + 32.0;
+	}
+
+	/**
+	 * Kpa to psi.
+	 *
+	 * @param value the value
+	 * @return the double
+	 */
+	public static Double kpaToPsi(Double value) {
+		return value * 0.14503773773020923;
+	}
+
+	/**
+	 * Kph to mph.
+	 *
+	 * @param value the value
+	 * @return the double
+	 */
+	public static Double kphToMph(Double value) {
+		return value * 0.621371192;
+	}
+	
+	/**
+	 * Km to mi.
+	 *
+	 * @param value the value
+	 * @return the double
+	 */
+	public static Double kmToMi(Double value) {
+		return value * 0.621371192;
+	}
+	
+	/**
+	 * Pa to psi.
+	 *
+	 * @param value the value
+	 * @return the double
+	 */
+	public static Double paToPsi(Double value) {
+		return value * 0.00014503773773020923;
+	}
+}
