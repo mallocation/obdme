1832888db9ca218977308593008e23a2b3786634
Curtis Johnson <curtisdavidjohnson@gmail.com>
2011-04-14 06:16:36 +0000

    WEB-41

--- web_app/obdmeweb/app/controllers/Charts.java
+++ web_app/obdmeweb/app/controllers/Charts.java
@@ -6,26 +6,32 @@ import java.util.List;
 
 import models.obdmedb.obd.ObdPid;
 import models.obdmedb.statistics.VehicleDataPoint;
+import models.obdmedb.statistics.VehicleDataPoint.LatestDataPoint;
+import models.obdmedb.vehicles.Vehicle;
 import play.mvc.Controller;
 import play.mvc.With;
 
 @With(Secure.class)
 public class Charts extends Controller {
 
-    public static void index() {    	
-		List<Double> values = VehicleDataPoint.getLatestValuesForPid(ObdPid.getObdPid("01", "3C"), "1HGFA16537L077259", 500);
-    	StringBuffer sb = new StringBuffer();
-    	sb.append("[");
-    	for (Double val : values) {
-    		sb.append(val.doubleValue() + ",");
-    	}
-    	sb.deleteCharAt(sb.length() - 1);
-    	sb.append("]");
-    	String data = sb.toString();
-    	int numPoints = values.size();
+    public static void index() {
+    	//List<LatestDataPoint> queryResults = VehicleDataPoint.selectLatestDataPoints(ObdPid.CALCULATED_ENGINE_LOAD_VALUE, Vehicle.findById(1L), 200);
+   	
+    	
+//		List<Double> values = VehicleDataPoint.getLatestValuesForPid(ObdPid.getObdPid("01", "3C"), "1HGFA16537L077259", 2);
+    //	StringBuffer sb = new StringBuffer();
+    	//sb.append("[");
+//    	for (Double val : values) {
+//    		sb.append(val.doubleValue() + ",");
+//    	}
+//    	sb.deleteCharAt(sb.length() - 1);
+    	//sb.append("]");
+    	//String data = sb.toString();
+//    	int numPoints = values.size();
     	List<ObdPid> pidList = new ArrayList<ObdPid>();
     	Collections.addAll(pidList, ObdPid.DEFINED_OBD_PIDS);
-		render(data, numPoints, pidList);
+		//render(data, numPoints, pidList);
+    	render(pidList);
 	}
 
 }
--- web_app/obdmeweb/app/controllers/Trips.java
+++ web_app/obdmeweb/app/controllers/Trips.java
@@ -1,9 +1,13 @@
 package controllers;
 
+import java.text.NumberFormat;
 import java.util.ArrayList;
 import java.util.List;
 
+import javax.swing.text.NumberFormatter;
+
 import models.obdmedb.User;
+import models.obdmedb.statistics.VehicleDataPoint;
 import models.obdmedb.trips.Trip;
 import play.libs.XML;
 import play.mvc.Before;
@@ -15,12 +19,16 @@ import play.templates.Template;
 @With(Secure.class)
 public class Trips extends Controller {
 	
-	public static class IndexTripBinding {
+	public static class IndexTripBinding {		 
 		public Long tripId;
 		public String tripName;
-		public IndexTripBinding(Long id, String name) {
+		public String dataPoints;
+		public IndexTripBinding(Long id, String name, Long dataPoints) {
 			this.tripId = id;
-			this.tripName = name;
+			this.tripName = name;			
+			NumberFormat nf = NumberFormat.getNumberInstance();
+			nf.setGroupingUsed(true);
+			this.dataPoints = nf.format(dataPoints);		
 		}
 	}
 	
@@ -31,7 +39,7 @@ public class Trips extends Controller {
     	List<Trip> userTrips = Trip.getLatestTripsForUser(user, 5);
     	List<IndexTripBinding> trips = new ArrayList<IndexTripBinding>();
     	for (Trip userTrip : userTrips) {
-    		trips.add(new IndexTripBinding(userTrip.getId(), userTrip.getAlias()));
+    		trips.add(new IndexTripBinding(userTrip.getId(), userTrip.getAlias(), VehicleDataPoint.selectDataPointCountForTrip(userTrip)));
     	} 	
     	render(trips);
 	}
